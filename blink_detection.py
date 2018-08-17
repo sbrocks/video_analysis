f4ab144a28bcc1cae4b4c53e702d248f4ab5a61e
Shaury <shaury29.baranwal@gmail.com>
2018-08-17 13:26:02 +0530

    added face ui and enthusiasm

--- blink_detection.py
+++ blink_detection.py
@@ -77,6 +77,34 @@ def calculate_EAR(eye):
         print("Error in eye shape")
         return -1
 
+def calculate_frown(l_eyebrow,r_eyebrow):
+    if len(l_eyebrow)==5 and len(r_eyebrow)==5:
+        frown_width = dist(l_eyebrow[0],r_eyebrow[4])
+        return frown_width
+    else:
+        print("Error in eyebrow shape")
+        return -1
+
+def calculate_mouth(mouth):
+    if len(mouth)==20:
+        mouth_dist = dist(mouth[14],mouth[18])
+        return mouth_dist
+    else:
+        print("Error in mouth shape")
+        return -1
+
+def calculate_enthusiasm(l_eye,r_eye,l_eyebrow,r_eyebrow):
+    if len(l_eye)==6 and len(r_eye)==6 and len(l_eyebrow)==5 and len(r_eyebrow)==5:
+        r_enthu_dist = dist(r_eye[2],r_eyebrow[3])
+        l_enthu_dist = dist(l_eye[2],l_eyebrow[3])
+        enthusiasm_dist = (r_enthu_dist+l_enthu_dist)/2.0
+        return enthusiasm_dist
+    else:
+        print("Error in eye or eyebrow shape")
+        return -1
+
+
+    
 
 def get_eyes(features):
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
@@ -86,14 +114,56 @@ def get_eyes(features):
     r_eye = features[rStart:rEnd]
     return l_eye, r_eye
 
-"""def get_eyebrows(features):
-    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+def get_eyebrows(features):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
     features = shape2np(features)
-    l_eye = features[lStart:lEnd]
-    r_eye = features[rStart:rEnd]
-    return l_eye, r_eye
-"""
+    l_eyebrow = features[lStart:lEnd]
+    r_eyebrow = features[rStart:rEnd]
+    #print(l_eyebrow)
+    #print(r_eyebrow)
+    return l_eyebrow, r_eyebrow
+
+def get_mouth(features):
+    (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+    features = shape2np(features)
+    mouth = features[mStart:mEnd]
+    #print(mouth)
+    return mouth
+
+
+def face_ui(frame,features):
+    (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+    (rebStart, rebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
+    (lebStart, lebEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
+    (reStart, reEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    (leStart, leEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+    (jStart, jEnd) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
+
+    features = shape2np(features)
+    mouth = features[mStart:mEnd]
+    right_eyebrow = features[rebStart:rebEnd]
+    left_eyebrow = features[lebStart:lebEnd]
+    right_eye = features[reStart:reEnd]
+    left_eye = features[leStart:leEnd]
+    nose = features[nStart:nEnd]
+    jaw = features[jStart:jEnd]
+    cv2.line(frame, (left_eye[0][0],left_eye[0][1]), (left_eye[2][0],left_eye[2][1]), (127, 0, 255),1)
+    cv2.line(frame, (left_eye[2][0],left_eye[2][1]), (left_eye[3][0],left_eye[3][1]), (127, 0, 255),1)
+    cv2.line(frame, (left_eye[3][0],left_eye[3][1]), (left_eye[4][0],left_eye[4][1]), (127, 0, 255),1)
+    cv2.line(frame, (left_eye[4][0],left_eye[4][1]), (left_eye[0][0],left_eye[0][1]), (127, 0, 255),1)
+    cv2.line(frame, (right_eye[0][0],right_eye[0][1]), (right_eye[1][0],right_eye[1][1]), (127, 0, 255),1)
+    cv2.line(frame, (right_eye[1][0],right_eye[1][1]), (right_eye[3][0],right_eye[3][1]), (127, 0, 255),1)
+    cv2.line(frame, (right_eye[3][0],right_eye[3][1]), (right_eye[5][0],right_eye[5][1]), (127, 0, 255),1)
+    cv2.line(frame, (right_eye[5][0],right_eye[5][1]), (right_eye[0][0],right_eye[0][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[0][0],mouth[0][1]), (right_eye[5][0],right_eye[5][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[6][0],mouth[6][1]), (left_eye[4][0],left_eye[4][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[0][0],mouth[0][1]), (nose[6][0],nose[6][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[6][0],mouth[6][1]), (nose[6][0],nose[6][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[0][0],mouth[0][1]), (mouth[9][0],mouth[9][1]), (127, 0, 255),1)
+    cv2.line(frame, (mouth[6][0],mouth[6][1]), (mouth[9][0],mouth[9][1]), (127, 0, 255),1)
+
 
 
 face_landmark_path = 'shape_predictor_68_face_landmarks.dat'
@@ -103,6 +173,9 @@ predictor = dlib.shape_predictor(face_landmark_path)
 
 cam = cv2.VideoCapture(0)
 
+num_frames=0
+frown_count=0
+enthusiasm_count=0
 frame_no=0
 x=list()
 y=list()
@@ -111,14 +184,33 @@ TOTAL_TIME_VIDEO = 0
 while True:
     ret, img = cam.read()
     TOTAL_TIME_VIDEO = cam.get(cv2.CAP_PROP_POS_MSEC)
-    
+    num_frames+=1
     img, rects, feature_array = find_features(img)
     n_faces = len(rects)
     if n_faces!=0:
         features = feature_array[0]         # Currently only calculating blink for the First face
         l_eye, r_eye = get_eyes(features)
+        l_eyebrow,r_eyebrow = get_eyebrows(features)
+        mouth = get_mouth(features)
+        
+        # Make UI for Face
+        face_ui(img,features)
+
         l_EAR = calculate_EAR(l_eye)
         r_EAR = calculate_EAR(r_eye)
+
+        frown_dist = calculate_frown(l_eyebrow,r_eyebrow)
+        mouth_dist = calculate_mouth(mouth)
+        enthusiasm_dist = calculate_enthusiasm(l_eye,r_eye,l_eyebrow,r_eyebrow)
+
+        #print("Enthusiasm:"+str(enthusiasm_dist))
+        if mouth_dist<12.0 and enthusiasm_dist>20.0:
+            enthusiasm_count+=1
+
+        #print("Frown Dist:"+str(frown_dist))
+        if frown_dist < 16.0:
+            frown_count+=1
+
         L_COUNTER += l_EAR <= EYE_AR_THRESH
         R_COUNTER += r_EAR <= EYE_AR_THRESH
 
@@ -148,12 +240,19 @@ while True:
         L_BLINK_COUNTER = 0
         R_BLINK_COUNTER = 0
 
-TOTAL_TIME_VIDEO = int(TOTAL_TIME_VIDEO/1000)
-TOTAL_BLINK_COUNTER = int(TOTAL_BLINK_COUNTER/2)
-print(TOTAL_BLINK_COUNTER)
-print(TOTAL_TIME_VIDEO)
 
-TOTAL_TIME_VIDEO_SECS = TOTAL_TIME_VIDEO/60
+# Frown Calculations
+print("Frown Percentage: "+str(round((frown_count/num_frames*100),2))+"%")
+print("Enthusiasm Percentage: "+str(round((enthusiasm_count/num_frames*100),2))+"%")
+
+# Blink Calculations
+TOTAL_TIME_VIDEO_SECS = int(TOTAL_TIME_VIDEO/1000)
+TOTAL_BLINK_COUNTER = int(TOTAL_BLINK_COUNTER/2)
+#print(TOTAL_BLINK_COUNTER)
+#print(TOTAL_TIME_VIDEO_SECS)
+
+TOTAL_TIME_VIDEO_MINS = TOTAL_TIME_VIDEO_SECS/60
+TOTAL_TIME_VIDEO_EXTRA_SECS = TOTAL_TIME_VIDEO_SECS%60
 
 # Ideal Blinking Rate is 15-20 blinks (per minute)
 # 15 blinks : Normal , Nervous : Very low
